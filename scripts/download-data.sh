b1d3c1cdf2a7ef91d77fd1d4095928281741afa3
Uira Caiado <ucaiado@msn.com>
2021-03-29 22:30:30 -0300

    docs: update script to download data

--- scripts/download-data.sh
+++ scripts/download-data.sh
@@ -4,7 +4,7 @@ pushd .
 cd ../CppND-Capstone/data
 [ -e *.csv ] && rm *.csv
 [ -e *.zip ] && rm *.zip
-wget -q https://www.dropbox.com/s/gbf43gdpl6gj57j/cppnd_capstone_data.zip
+wget -q https://www.dropbox.com/s/a23cezde4494gdy/cppnd_capstone_data.zip
 unzip cppnd_capstone_data.zip
 rm cppnd_capstone_data.zip
 mv data/*.csv .
